9d3bafb87ae2a5454d3911afe37a8d85d1b57c5f
Muhammad Yasir <muhammadyasir@pk.folio3.com>
2022-11-30 21:38:12 +0500

    Updated App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from "react";
+import {
+  appName,
+  userName,
+  githubUserName,
+} from "./constants/GenericConstants";
+import { headerStyle } from "./asserts/StyleAsserts";
 
 const App = () => {
   return (
     <div>
-      <header>
-        <nav>App name here</nav>
+      <header style={headerStyle}>
+        <h1>{appName}</h1>
       </header>
+
       <hr />
-      <h1>Welcome name</h1>
+      <h1>
+        Welcome <a target="_blank" href={`https://github.com/${githubUserName}/`}> {userName} </a>
+      </h1>
     </div>
   );
 };
